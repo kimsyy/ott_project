4369027ba0adbd52d3043c91d644f713f14803d3
kimsyy <kimse7530@daum.net>
2023-08-06 14:59:28 +0900

    api 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "./api/axios";
+import requests from "./api/request";
 
 function App() {
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const request = await axios.get(requests.fetchNowPlaying);
+    console.log(request);
+  };
   return <div className="App">hi</div>;
 }
 
---
+++ src/api/request.ts
@@ -0,0 +1,5 @@
+const requests = {
+  fetchNowPlaying: "movie/now_playing"
+};
+
+export default requests;
